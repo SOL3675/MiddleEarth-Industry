3235bd97adb9177b8af27d545d223dd474ed011a
SOL3675 <sol3675.at.crafters@gmail.com>
2018-12-28 14:56:08 +0900

    Add GuiHandler

--- src/main/java/sol3675/middleearthindustry/proxy/CommonProxy.java
+++ src/main/java/sol3675/middleearthindustry/proxy/CommonProxy.java
@@ -3,7 +3,10 @@ package sol3675.middleearthindustry.proxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import sol3675.middleearthindustry.MiddleEarthIndustry;
 import sol3675.middleearthindustry.common.MeiContents;
+import sol3675.middleearthindustry.common.gui.GuiHandler;
 import sol3675.middleearthindustry.config.MeiCfg;
 
 public class CommonProxy {
@@ -17,6 +20,7 @@ public class CommonProxy {
     public void init(FMLInitializationEvent event)
     {
         MeiContents.init();
+        NetworkRegistry.INSTANCE.registerGuiHandler(MiddleEarthIndustry.instance, new GuiHandler());
     }
 
     public void postInit(FMLPostInitializationEvent event)
--- src/main/java/sol3675/middleearthindustry/references/Constant.java
+++ src/main/java/sol3675/middleearthindustry/references/Constant.java
@@ -1,4 +1,8 @@
 package sol3675.middleearthindustry.references;
 
 public class Constant {
+
+    //GUI ID
+    public static int GUI_AUTO_CRAFTING_TABLE = 0;
+
 }
